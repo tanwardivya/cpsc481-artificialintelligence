7cb404be6b65b1d96506bdf5caac440e0c1bd70b
tanwardivya <divyatanwar26@gmail.com>
2023-06-09 22:30:32 -0700

    Initial Commit

---
+++ misscannibalpuzzle/misscannibalpuzzle/misscannibals.py
@@ -0,0 +1,40 @@
+from search import *
+
+class MissCannibals(Problem):
+    def __init__(self, M=3, C=3, goal=(0, 0, False)):
+        initial = (M, C, True)
+        self.M = M
+        self.C = C
+        super().__init__(initial, goal)
+
+    def actions(self, state):
+        """Return the actions that can be executed in the given
+        state. The result would typically be a list, but if there are
+        many actions, consider yielding them one at a time in an
+        iterator, rather than building them all at once."""
+        raise NotImplementedError
+
+    def result(self, state, action):
+        """Return the state that results from executing the given
+        action in the given state. The action must be one of
+        self.actions(state)."""
+        raise NotImplementedError
+
+    def goal_test(self, state):
+        """Return True if the state is a goal. The default method compares the
+        state to self.goal or checks for state in self.goal if it is a
+        list, as specified in the constructor. Override this method if
+        checking against a single self.goal is not enough."""
+        if isinstance(self.goal, list):
+            return is_in(state, self.goal)
+        else:
+            return state == self.goal
+
+    # YOUR CODE GOES HERE
+if __name__ == '__main__':
+    mc = MissCannibals(3,3)
+    #print(mc.actions((3, 2, True))) # Test your code as you develop! This shoul return ['CC', 'C', 'M']
+    path = depth_first_graph_search(mc).solution()
+    print(path)
+    path = breadth_first_graph_search(mc).solution()
+    print(path)
